28d3f477cd719d8a7333ac4e3831901f8aa772d6
nththuha <thuhango0204@gmail.com>
2025-05-17 16:24:15 +0700

    chore: update ui

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,11 +1,19 @@
 import { Button, Stack, Text, Textarea } from '@mantine/core'
-import { useCallback, useState } from 'react'
+import { useForm } from '@mantine/form'
+import { useCallback } from 'react'
+
+type FormProps = {
+  html: string
+}
 
 export default function Home() {
-  const [value, setValue] = useState('')
+  const form = useForm<FormProps>({
+    initialValues: { html: '' },
+    validate: { html: (value: string) => (value === '' ? 'Không được để trống' : null) },
+  })
 
-  const onClick = useCallback(() => {
-    const cleaned = value
+  const submit = useCallback((values: FormProps) => {
+    const cleaned = values.html
       .replace(/<script>\s*jQuery\('.*?\.hidden_content'\)\.html\(''\);\s*<\/script>/g, '')
       .replace(
         /<div class="hidden_content"[^>]*style=["']?display:\s*none;?["']?[^>]*>/g,
@@ -17,25 +25,26 @@ export default function Home() {
     const blob = new Blob([cleaned], { type: 'text/html' })
     const url = URL.createObjectURL(blob)
     window.open(url, '_blank')
-  }, [value])
+  }, [])
 
   return (
-    <Stack align="center" justify="center" h="100dvh">
-      <Stack gap={10} w={600}>
-        <Text fz={32} fw="bold">
-          Welcome back, Gia Hân!
-        </Text>
-        <Textarea
-          value={value}
-          onChange={(event) => setValue(event.currentTarget.value)}
-          label="Nhập HTML"
-          placeholder={`<html lang="vi">...</html>`}
-          autosize
-          minRows={10}
-          maxRows={10}
-        />
-        <Button onClick={onClick}>Submit</Button>
-      </Stack>
+    <Stack align="center" justify="center" h="100dvh" w="100vw">
+      <form onSubmit={form.onSubmit(submit)}>
+        <Stack gap={10}>
+          <Text fz={32} fw="bold">
+            Welcome back, Gia Hân!
+          </Text>
+          <Textarea
+            label="Nhập HTML"
+            placeholder={`<html lang="vi">...</html>`}
+            autosize
+            minRows={10}
+            maxRows={10}
+            {...form.getInputProps('html')}
+          />
+          <Button type="submit">Submit</Button>
+        </Stack>
+      </form>
     </Stack>
   )
 }
